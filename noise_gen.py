6b2ae8700982d4e35e7600f1c58fc55beea95d7b
vasudeva43 <131653054+vasudeva43@users.noreply.github.com>
2024-10-21 17:56:22 +0530

    minor changes

--- noise_gen.py
+++ noise_gen.py
@@ -7,12 +7,12 @@ amp = 2
 RATE = 8000  # Add a sample rate definition (e.g., 8kHz)
 
 # Function to generate random noise
-def noise_gen(amp, length):
+def noise_gen(amp, length, Rate):
     test_noise = 2 * amp * (np.random.random(length) - 0.5)
     error_noise = 2 * amp * (np.random.random(length) - 0.5)
 
-    write("./modified/test_noise.wav", RATE, test_noise.astype(np.float32))
-    write("./modified/error_noise.wav", RATE, error_noise.astype(np.float32))
+    write("./modified/test_noise.wav", Rate, test_noise.astype(np.float32))
+    write("./modified/error_noise.wav", Rate, error_noise.astype(np.float32))
 
 # Call the function to generate noise
-noise_gen(amp, length)
+noise_gen(amp, lengthRATE)
