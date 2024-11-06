ae6267c06f196b484ca89c7346be8e374649bd97
vasudeva43 <131653054+vasudeva43@users.noreply.github.com>
2024-11-06 22:40:23 +0530

    chk this

--- freq_analysis.py
+++ freq_analysis.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
+import numpy as np
 
-f, t, Sxx = spectrogram(test_signal, RATE)
-plt.pcolormesh(t, f, 10 * np.log10(Sxx))
+# Define the paths for each signal
+signal_paths = ['path/to/signal1', 'path/to/signal2', 'path/to/signal3']
+
+# Load the signals from the paths
+signals = [np.load(path) for path in signal_paths]
+labels = ['Signal 1', 'Signal 2', 'Signal 3']
+colors = ['viridis', 'plasma', 'inferno']
+
+plt.figure(figsize=(10, 6))
+
+for i, signal in enumerate(signals):
+    f, t, Sxx = spectrogram(signal, RATE)
+    plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap=colors[i], alpha=0.5, label=labels[i])
+
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
-plt.title('Spectrogram of Test Signal')
+plt.title('Spectrogram of Multiple Signals')
+plt.legend()
+plt.colorbar(label='Intensity [dB]')
 plt.show()
